5a8d6c8094858ebf88d1a477195858780b0918c9
bhargava-adi <bhargava.adi.s@gmail.com>
2022-09-21 21:23:30 +0530

    Initial Project Configuration Stage

---
+++ src/main/java/org/spring/boot/jwt/SpringBootJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.spring.boot.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spring/boot/jwt/SpringBootJwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.boot.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
